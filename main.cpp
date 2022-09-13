a241bb27b2826ed984b83b9b050a385d00a17e54
cengiz <onkal.cengiz@gmail.com>
2022-09-13 22:03:53 +0300

    print

--- main.cpp
+++ main.cpp
@@ -34,38 +34,53 @@ public:
 
 class Narrative {
 public:
-    int id{};
-    //text to show
-    std::string text{};
-    //has options
+    int id;
+    std::string text;
     std::vector<Option> options;
+
 
     void print() {
         std::cout << text << std::endl;
         //print options
-        for (auto &option: options) {
-            option.print();
+        for (int i = 0; i < options.size(); i++) {
+            std::cout << i << ". ";
+            options[i].print();
         }
+
+
     }
 
 };
 
 class Story {
     //has narrative list
-    std::list<Narrative> narratives;
-
+    std::vector<Narrative> narratives;
+public:
     void loadNarratives() {
         sqlite3 *db;
-        sqlite3_open("story.db", &db);
+        sqlite3_open("kuest.db", &db);
+
+
         sqlite3_stmt *stmt;
-        sqlite3_prepare_v2(db, "SELECT id,text FROM narrative", -1, &stmt, NULL);
+        sqlite3_prepare_v2(db, "SELECT id,narrative FROM narratives order by id", -1, &stmt, nullptr);
         while (sqlite3_step(stmt) == SQLITE_ROW) {
             Narrative n;
-            n.id = sqlite3_column_int(stmt, 1);
-            n.text = (char *) sqlite3_column_text(stmt, 2);
+            n.id = sqlite3_column_int(stmt, 0);
+            n.text= reinterpret_cast<const char *>(sqlite3_column_text(stmt, 1));
             //load options
             sqlite3_stmt *stmt2;
-            sqlite3_prepare_v2(db, "SELECT text,required_skill,dc,success,failure FROM option WHERE narrative_id = ?", -1, &stmt2, NULL);
+            sqlite3_prepare_v2(db, "SELECT text, required_skill, dc, success, fail FROM options WHERE narrative_id = ?",
+                               -1, &stmt2, nullptr);
+            sqlite3_bind_int(stmt2, 1, n.id);
+            Option o;
+            while (sqlite3_step(stmt2) == SQLITE_ROW) {
+                o.text = std::string(reinterpret_cast<const char *>(sqlite3_column_text(stmt2, 0)));
+                o.required_skill = (Skills) sqlite3_column_int(stmt2, 1);
+                o.dc = sqlite3_column_int(stmt2, 2);
+                o.success = getNarrative(sqlite3_column_int(stmt2, 3));
+                o.failure = getNarrative(sqlite3_column_int(stmt2, 4));
+                n.options.push_back(o);
+            }
 
             narratives.push_back(n);
 
@@ -74,19 +89,34 @@ class Story {
         sqlite3_close(db);
     }
 
+    Narrative *getNarrative(int id) {
+        return &narratives[id];
+    }
+
 //load from database
 };
 
 class Player {
-    //current narrative
-    Narrative *narrative;
-    //skills
+
+public:
+    Narrative *currentNarrative;
     int skills[6];
+
+    void setCurrentNarrative(Narrative *n) {
+        currentNarrative = n;
+    }
 
 };
 
 
 int main() {
+
+    Story story;
+    story.loadNarratives();
+
+    Player player;
+    player.setCurrentNarrative(story.getNarrative(0));
+    player.currentNarrative->print();
 
     return 0;
 }
